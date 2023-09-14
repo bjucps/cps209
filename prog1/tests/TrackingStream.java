3b4604ff94fd9b2d1ef8bf6a28d35f4571014024
Dr. Sarah <32044936+doctorsarah@users.noreply.github.com>
2023-09-14 13:10:35 -0400

    Update TrackingStream.java
    Added handling for different end-of-line characters.

--- prog1/tests/TrackingStream.java
+++ prog1/tests/TrackingStream.java
@@ -14,14 +14,75 @@ class TrackingStream extends PrintStream {
     }
 
     @Override
+    public void print(String str) {
+        String toPrint = standardizeEOLcharacters(str);
+        super.print(toPrint);
+        printed += toPrint;
+    }   
+
+    @Override
+    public void println(String str) {
+        print(str + System.lineSeparator());
+    }   
+
+    @Override
     public void print(Object obj) {
-        super.print(obj);
-        printed += obj;
+        print(obj.toString());
     }   
 
     @Override
     public void println(Object obj) {
-        super.println(obj);
-        printed += obj + System.lineSeparator();
+        println(obj.toString());
     }   
+
+    private static String standardizeEOLcharacters (Object objToPrint) {
+        StringBuilder sbToPrint = new StringBuilder(objToPrint.toString());
+        correctBackslashRs(sbToPrint);
+        correctBackslashNs(sbToPrint);
+        correctBackslashRNs(sbToPrint);
+        return sbToPrint.toString();
+    }
+
+    private static void correctBackslashRs(StringBuilder sbToPrint) {
+        final String EOL_CHAR = "\r";
+        if (!System.lineSeparator().equals(EOL_CHAR)) {
+            int loc = sbToPrint.indexOf(EOL_CHAR, 0);
+            int lengthDiff = System.lineSeparator().length() - EOL_CHAR.length();
+            while (loc >= 0) {
+                if (loc == sbToPrint.length() - 1 || sbToPrint.charAt(loc + 1) != '\n') {
+                    sbToPrint.deleteCharAt(loc);
+                    sbToPrint.insert(loc, System.lineSeparator());
+                }
+                loc = sbToPrint.indexOf(EOL_CHAR, loc + lengthDiff);
+            }
+        }
+    }
+
+    private static void correctBackslashNs(StringBuilder sbToPrint) {
+        final String EOL_CHAR = "\n";
+        if (!System.lineSeparator().equals(EOL_CHAR)) {
+            int loc = sbToPrint.indexOf(EOL_CHAR, 0);
+            int lengthDiff = System.lineSeparator().length() - EOL_CHAR.length();
+            while (loc >= 0) {
+                if (loc == 0 || sbToPrint.charAt(loc - 1) != '\r') {
+                    sbToPrint.deleteCharAt(loc);
+                    sbToPrint.insert(loc, System.lineSeparator());
+                }
+                loc = sbToPrint.indexOf(EOL_CHAR, loc + lengthDiff);
+            }
+        }
+    }
+
+    private static void correctBackslashRNs(StringBuilder sbToPrint) {
+        final String EOL_CHAR = "\r\n";
+        if (!System.lineSeparator().equals(EOL_CHAR)) {
+            int loc = sbToPrint.indexOf(EOL_CHAR, 0);
+            int lengthDiff = System.lineSeparator().length() - EOL_CHAR.length();
+            while (loc >= 0) {
+                sbToPrint.deleteCharAt(loc);
+                sbToPrint.insert(loc, System.lineSeparator());
+                loc = sbToPrint.indexOf(EOL_CHAR, loc + lengthDiff);
+            }
+        }
+    }
 }
