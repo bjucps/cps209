255ae10593de8f19c12829bf01a72ab88db01d95
Dr. Sarah <32044936+doctorsarah@users.noreply.github.com>
2023-09-15 09:56:00 -0400

    Update App.java
    Change in strategy.

--- prog1/src/App.java
+++ prog1/src/App.java
@@ -1,21 +1,6 @@
-import java.io.PrintStream;
-
-/**
- * Starting app program. Uses a variable to store the output object.
- *     out variable can be assigned to other objects for testing.
- */
 public class App {
-    public static PrintStream out = System.out;
-
-    public static void print(Object obj) {
-        out.print(obj);
-    } 
-
-    public static void println(Object obj) {
-        out.println(obj);
-    } 
-        
     public static void main(String[] args) {
-        println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.err.println("Errors go here.");
     }
 }
