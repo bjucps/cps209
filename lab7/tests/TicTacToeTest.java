8ba8a8d657af38c293903c4ced5da96ce5eec704
Sarah Gothard <sgothard@bju.edu>
2024-03-07 10:24:41 -0500

    Updated Testing imports

--- lab7/tests/TicTacToeTest.java
+++ lab7/tests/TicTacToeTest.java
@@ -1,7 +1,6 @@
-package model;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTest {
 
