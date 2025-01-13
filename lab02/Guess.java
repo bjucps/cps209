86e59c534570b62c6e5bf29570a08d2482f1ab8f
sgothard <sgothard@bju.edu>
2025-01-13 14:05:53 -0500

    Blank starting point

--- lab02/Guess.java
+++ lab02/Guess.java
@@ -1,35 +1,10 @@
 package app;
 
-import java.util.Random;
-
-
 public class Guess {
-    private int secret;
-
-    public Guess() {
-        var rand = new Random();
-        secret = rand.nextInt(10);
-    }
-
-    public Guess(int secret) {
-        this.secret = secret;
-    }
-
-    public String check(int guess) {
-        if (guess < secret) {
-            return "Too Low.";
-        } else if (guess >= secret) {
-            return "Too High.";
-        } else {
-            return "Correct!";
-        }
-    }
-
-    public int getSecret() {
-        return secret;
-    }
-
-    public void setSecret(int newSecret) {
-        secret = newSecret;
-    }
+    // Goals: 
+    // * constructor that picks a random number [1, 10]
+    // * constructor that sets the secret number to parameter value
+    // * get/set methods for secret
+    // * check method that provides String feedback on a guess
+    // * set method for secret throws an IllegalArgumentException if the secret is < 1 or > 10
 }
--- lab02/GuessTest.java
+++ lab02/GuessTest.java
@@ -6,12 +6,7 @@ import org.junit.jupiter.api.Test;
 public class GuessTest
 {  
     @Test
-    public void testCheck_LowGuess_ReportsTooLow()
-    {
-        var g = new Guess();
-        g.setSecret(5);
-        String result = g.check(7);
-        assertEquals("Too Low.", result);
+    public void test_set_secret() {
     }
 
 }
