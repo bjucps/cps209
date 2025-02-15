acbcd95883873edc3ee5ac0d367cb8da55e49f24
Dr. Sarah <32044936+doctorsarah@users.noreply.github.com>
2025-02-15 09:24:06 -0500

    Alternate testing with TrackingStream
    2 methods of black-box testing: TrackingStream and ProcessBuilder.

--- prog2/AppTest.java
+++ prog2/AppTest.java
@@ -11,8 +11,80 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
-/* To use ProcessBuilder, please add the following lines to the bottom of build.gradle:
 
+public class AppTest {
+
+    /**
+     * Testing with TrackingStream
+     */
+    @Test
+    public void test_80_main_normal() {
+        TrackingStream outStream = new TrackingStream();
+        System.setOut(outStream);
+        System.setErr(outStream);
+        String [] args =  {"ice.png"};
+        App.main(args);
+        assertTrue(outStream.printed.contains("Metadata in ice.png:"));
+        assertTrue(outStream.printed.contains("Software: "));
+        assertTrue(outStream.printed.contains("Signature: 3248a69c033c15e46356a9ecb996c652"));
+        assertTrue(outStream.printed.contains("Delay: 42"));
+    }
+
+    @Test
+    public void test_80_main_sample_png() {
+        TrackingStream outStream = new TrackingStream();
+        System.setOut(outStream);
+        System.setErr(outStream);
+        String [] args = {"sample.png"};
+        App.main(args);
+        assertTrue(outStream.printed.contains("Metadata in sample.png:"));
+        assertTrue(outStream.printed.contains("Software: "));
+    }
+
+    @Test
+    public void test_80_main_no_args() {
+        TrackingStream outStream = new TrackingStream();
+        System.setOut(outStream);
+        System.setErr(outStream);
+        String [] args = {};
+        App.main(args);
+        assertTrue(outStream.printed.contains("Usage: Java App [filename] or arun [filename]"));
+    }
+
+    /**
+     * LongMethodChecker is available in the prog1 folder.
+     */
+    @Test
+    public void test_80_method_too_long() {
+        File f = new File(System.getProperty("user.dir"));
+        LongMethodChecker.checkFiles(f.listFiles());
+    }
+
+    @Test
+    public void test_a105_modify_metadata() {
+        TrackingStream outStream = new TrackingStream();
+        System.setOut(outStream);
+        System.setErr(outStream);
+        String [] args = {"sample.png", "Software", "BJU-CpS-209"};
+        App.main(args);
+        assertTrue(outStream.printed.contains("Metadata in sample.png:"));
+        assertTrue(outStream.printed.contains("Software: BJU-CpS-209"));
+    }
+
+    @Test
+    public void test_a105_modify_metadata_fail() {
+        TrackingStream outStream = new TrackingStream();
+        System.setOut(outStream);
+        System.setErr(outStream);
+        String [] args = {"sample.png", "Denomination", "Presbyterian"};
+        App.main(args);
+        assertTrue(outStream.printed == null || outStream.printed.equals("") || outStream.printed.startsWith("No Denomination found"));
+    }
+
+
+
+/* Testing with Process Builder. 
+To use tests with ProcessBuilder, please add the following lines to the bottom of build.gradle:
 
 jar {
   manifest {
@@ -23,14 +95,8 @@ jar {
 }
 
 */
-
-public class AppTest {
-
-    /**
-     * Runs the program like the user would.
-     */
     @Test
-    public void test_80_main_normal() {
+    public void test_80_main_normal_processBuilder() {
         System.out.println("User dir: " + System.getProperty("user.dir"));
         try {
             Process process = new ProcessBuilder("java",
@@ -52,7 +118,7 @@ public class AppTest {
     }
 
     @Test
-    public void test_80_main_sample_png() {
+    public void test_80_main_sample_png_processBuilder() {
         try {
             Process process = new ProcessBuilder("java",
                     "-jar", "build/libs/app.jar", "sample.png").start();
@@ -70,7 +136,7 @@ public class AppTest {
     }
 
     @Test
-    public void test_80_main_no_args() {
+    public void test_80_main_no_args_processBuilder() {
         String line;
         try {
             Process process = new ProcessBuilder("java",
@@ -100,13 +166,13 @@ public class AppTest {
      * LongMethodChecker is available in the prog1 folder.
      */
     @Test
-    public void test_80_method_too_long() {
+    public void test_80_method_too_long_processBuilder() {
         File f = new File(System.getProperty("user.dir"));
         LongMethodChecker.checkFiles(f.listFiles());
     }
 
     @Test
-    public void test_a105_modify_metadata() {
+    public void test_a105_modify_metadata_processBuilder() {
         try {
             Process process = new ProcessBuilder("java",
                     "-jar", "build/libs/app.jar", "sample.png", "Software", "BJU-CpS-209").start();
@@ -124,7 +190,7 @@ public class AppTest {
     }
 
     @Test
-    public void test_a105_modify_metadata_fail() {
+    public void test_a105_modify_metadata_fail_processBuilder() {
 
         try {
             String line = null;
